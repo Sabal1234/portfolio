9d31a81a84cf85d26b4eb748ff41204e9e53c682
Sabal Adhikari <sadaladhikari@gmail.com>
2025-10-10 09:16:19 +0545

    Update

--- src/components/Navbar/Hero/Hero.jsx
+++ src/components/Navbar/Hero/Hero.jsx
@@ -3,7 +3,13 @@ import './Hero.css'
 const Hero = () => {
   return (
       <div className='hero'>
-          
+      <img src="" alt="profile_img" />
+      <h1><span>I'm Sabal Adhikari,</span> frontend developer based in Nepal</h1>
+      <p>I am a frontend developer from Chabahil, Kathmandu with 2 years of experiece</p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My resume</div>
+      </div>
     </div>
   )
 }
