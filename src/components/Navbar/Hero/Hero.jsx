8a79e8dc304e5cc5424149d400e1313022445e32
Sabal Adhikari <sadaladhikari@gmail.com>
2025-10-13 06:42:11 +0545

    Update

--- src/components/Navbar/Hero/Hero.jsx
+++ src/components/Navbar/Hero/Hero.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
 import './Hero.css'
+import profile_img from '../../../assets/profile.jpg'
 const Hero = () => {
   return (
       <div className='hero'>
-      <img src="" alt="profile_img" />
+      <img src={profile_img} alt="profile_img" />
       <h1><span>I'm Sabal Adhikari,</span> frontend developer based in Nepal</h1>
       <p>I am a frontend developer from Chabahil, Kathmandu with 2 years of experiece</p>
       <div className="hero-action">
