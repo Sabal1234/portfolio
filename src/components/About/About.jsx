3010ec4cbeff05d5c912cd59959c55c19b22995e
Sabal Adhikari <sadaladhikari@gmail.com>
2025-10-14 14:11:15 +0545

    update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,19 +17,32 @@ const About = () => {
                       <p>My passion for frontend development is not reflectedin my extensive experience but also in the enthusiasm and dedication I bring to each peoject</p>
                   </div>
                   <div className="about-skills">
-                      <div className="about-skill">
-                          <p>HTML & CSS</p><hr style={{ width: "50%" }} />
-                          <p>React JS</p><hr style={{width:"70%"}} />
-                          <p>JavaScript</p><hr style={{width:"60%"}} />
-                          <p>Node JS</p><hr style={{width:"50%"}} />
-                          <p></p>
-                      </div>
+                      <div className="about-skill"><p>HTML & CSS</p><hr style={{ width: "50%" }} />     </div>
+                       <div className="about-skill">  <p>Tailwind CSS</p><hr style={{width:"60%"}} /></div>
+                         <div className="about-skill"> <p>React JS</p><hr style={{width:"70%"}} /></div>
+                       <div className="about-skill">  <p>JavaScript</p><hr style={{width:"60%"}} /></div>
+                         <div className="about-skill">  <p>Node JS</p><hr style={{width:"50%"}} /></div>
+                      
+                 
                   </div>
               </div>
           </div>
-        
+          <div className="about-achievements">
+              <div className="about-achievement">
+                  <h1>1</h1>
+                  <p>YEARS OF EXPERIENCE</p>
+              </div>
+              <hr />
+                <div className="about-achievement">
+                  <h1>20+</h1>
+                  <p>PROJECT COMPLETED</p>
+               
+              </div>
+                 
+  
+             
+          </div>
     </div>
   )
 }
-
 export default About
