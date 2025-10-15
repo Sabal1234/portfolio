8ed5e8cc4386f9c2ae9b7095b18e88b485edb498
Sabal Adhikari <sadaladhikari@gmail.com>
2025-10-15 18:44:51 +0545

    Update

---
+++ src/assets/myWork/mywork_data.js
@@ -0,0 +1,33 @@
+import movieSearch from './movieSearch.png'
+import noteManagement from './noteManagement.png'
+import rockpapergame from './rockpapergame.png'
+import todoList from './todoList.png'
+import youtubeclone from './youtubeclone.png'
+const mywork_data = [
+    {
+        w_no: 1,
+        w_name: "Search movie",
+        w_img:movieSearch
+    },
+        {
+        w_no: 2,
+        w_name: "Youtube Clone",
+        w_img:youtubeclone
+    },
+            {
+        w_no: 3,
+        w_name: "To-DO List",
+        w_img:todoList
+    },
+                {
+        w_no: 4,
+        w_name: "Note Management",
+        w_img:noteManagement
+    },
+                    {
+        w_no: 5,
+        w_name: "Rock Paper Scissors game",
+        w_img:rockpapergame
+    },
+]
+export default mywork_data;
--- src/components/MyWork/MyWork.jsx
+++ src/components/MyWork/MyWork.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import './MyWork.css'
+import mywork_data from '../../assets/myWork/mywork_data'
+
 const MyWork = () => {
   return (
-    <div>MyWork</div>
+      <div className='mywork'>
+          <div className="mywork-title">
+              <h1>My Work</h1>
+              <img src="" alt="errro"/>
+          </div>  
+          <div className="mywork-container">
+              {mywork_data.map((work,index) => {
+                  return <img key={ index} src={work. w_img} alt="image not shown"/>
+              })}
+          </div>
+
+    </div>
   )
 }
 
