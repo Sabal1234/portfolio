b659d016253bf14bc1d8404f06ad6ed7210eba00
Sabal Adhikari <sadaladhikari@gmail.com>
2025-10-16 21:48:37 +0545

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 const Navbar = () => {
+  const [menu,setMenu]=useState("home")
   return (
       <div className='navbar'>
           <img src="" alt="error" />
           <ul className='nav-menu'>
-              <li>Home</li>
-              <li>About Me</li>
-              <li>Services</li>
-              <li>Portfolio</li>
-              <li>Contact</li>
+        <li><p onClick={()=>setMenu("home")}>Home</p>{ menu==="home"?<img src='' alt='image not shown'/>:<></>}</li>
+              <li><p onClick={()=>setMenu("about")}>About Me</p>{ menu==="about"?<img src='' alt='image not shown'/>:<></>}</li>
+              <li><p onClick={()=>setMenu("services")}>Services</p>{ menu==="services"?<img src='' alt='image not shown'/>:<></>}</li>
+              <li><p onClick={()=>setMenu("work")}>Portfolio</p>{ menu==="home"?<img src='' alt='image not shown'/>:<></>}</li>
+              <li><p onClick={()=>setMenu("contact")}>Contact</p>{ menu==="home"?<img src='' alt='image not shown'/>:<></>}</li>
           </ul>
           <div className="nav-connect">Connect with me</div>
     </div>
